20e5eac1907e936aa387541fbdfce8a3758fe2ec
amanyoosuf <aryoosuf49@gmail.com>
2026-06-28 18:48:06 +0800

    Create Dropdown.js

---
+++ src/components/Dropdown.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { MenuItems } from './MenuItems';
+import './Dropdown.css';
+import { Link } from 'react-router-dom';
+
+function Dropdown({ onClose, closeMobileMenu }) {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => {
+    setClick(!click);
+  };
+
+  const handleLinkClick = () => {
+    setClick(false);
+    onClose && onClose();
+    // Close the main navbar menu and trigger scroll in mobile view
+    closeMobileMenu && closeMobileMenu();
+  };
+
+  return (
+    <>
+      <ul
+        onClick={handleClick}
+        className={click ? 'dropdown-menu clicked' : 'dropdown-menu'}
+      >
+        {MenuItems.map((item, index) => {
+          const isAnchorLink = item.path.startsWith('#');
+          
+          return (
+            <li key={index}>
+              {isAnchorLink ? (
+                <a
+                  className={item.cName}
+                  href={item.path}
+                  onClick={handleLinkClick}
+                >
+                  {item.title}
+                </a>
+              ) : (
+                <Link
+                  className={item.cName}
+                  to={item.path}
+                  onClick={handleLinkClick}
+                >
+                  {item.title}
+                </Link>
+              )}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
+
+export default Dropdown;
