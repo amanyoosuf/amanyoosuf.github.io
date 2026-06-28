e4fd76d5aa1286f2dd3b974552f599270b78d733
amanyoosuf <aryoosuf49@gmail.com>
2026-06-29 02:38:57 +0800

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "../App.css";
 import Cards from './Cards'; 
-import './mainCon.css';
+import './components/mainCon.css';
 import Footer from './Footer'; 
 
 function Home() {
