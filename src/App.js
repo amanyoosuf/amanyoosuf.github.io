3ff4baa6f9942e7b8f1110b512b65d327db6f9b4
amanyoosuf <aryoosuf49@gmail.com>
2026-07-26 14:35:39 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import React from 'react';
 import './App.css';
-import './components/mainCon.css';
-import Navbar from './components/navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-
-// Streamlined Layout feeding your clean single-page ecosystem
-const MainLayout = ({ children }) => (
-  <>
-    <Navbar />
-    {children}
-  </>
-);
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Main single-page portfolio routing anchor */}
-        <Route path="/" element={<MainLayout><Home /></MainLayout>} />
+        {/* Render Home directly since Home now contains its own clean minimalist sidebar/layout */}
+        <Route path="/" element={<Home />} />
         
-        {/* Fallback route: Redirects any accidental multi-page links safely back home */}
-        <Route path="*" element={<MainLayout><Home /></MainLayout>} />
+        {/* Fallback route: Redirects any broken links safely back home */}
+        <Route path="*" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
